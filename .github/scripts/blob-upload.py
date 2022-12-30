a57652d75f6d77a6d9979cf2ce6b321458322e28
Adon Metcalfe <adonm@fastmail.fm>
2022-12-30 18:01:27 +0800

    including some missed changes

--- .github/scripts/blob-upload.py
+++ .github/scripts/blob-upload.py
@@ -5,7 +5,7 @@ run("az config set extension.use_dynamic_install=yes_without_prompt", shell=True
 expiry = (datetime.utcnow() + timedelta(days=45)).isoformat().split("T")[0]
 sas = check_output(f"az storage container generate-sas --connection-string $AZURE_STORAGE_CONNSTRING --permissions lr --expiry {expiry} --name tlp-green -o tsv", shell=True).decode("utf-8").strip()
 
-for path in ["tlp=green/sas_link.txt", "tlp-green/reportcontent/soc-update.md"]:
+for path in ["tlp-green/sas_link.txt", "tlp-green/reportcontent/soc-update.md"]:
     content = open(path, "r").read()
     with open(path, "w") as f:
         f.write(content.replace("{sas}", sas))
