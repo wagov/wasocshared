547cfc68bfe0a816cf574ad8e812c866dbafa8a9
Adon Metcalfe <adon.metcalfe@dpc.wa.gov.au>
2023-06-08 05:54:48 +0000

    drop unused macro

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from itertools import groupby
 from dateutil.parser import parse
 
+
 def define_env(env):
     """
     This is the hook for defining variables, macros and filters
@@ -11,28 +12,6 @@ def define_env(env):
     - macro: a decorator function, to declare a macro.
     """
 
-
-    @env.macro
-    def include_file(filename, start_line=0, end_line=None, until=None):
-        """
-        Include a file, optionally indicating start_line and end_line
-        (start counting from 0)
-        The path is relative to the top directory of the documentation
-        project.
-        """
-        full_filename = os.path.join(env.project_dir, filename)
-        with open(full_filename, 'r') as f:
-            lines = f.readlines()
-        line_range_full = lines[start_line:end_line]
-        line_range = []
-        if until is not None:
-            for line in line_range_full:
-                if line.strip() != until.strip():
-                    line_range.append(line)
-        else:
-            line_range = line_range_full
-        return ''.join(line_range)
-    
     def get_title(path):
         line = ""
         with open(path) as f:
@@ -66,4 +45,4 @@ def define_env(env):
             if include is not None:
                 if month_count > include:
                     break
-        return("\n".join(mdtext))
+        return "\n".join(mdtext)
